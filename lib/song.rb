1c8c388ec5fbed2356040dc185801b5a255579dd
Nolan Hughes <nhughes987@gmail.com>
2018-01-16 01:15:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -65,7 +65,6 @@ class Song
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_name)
     song = self.new(song_name, artist, genre)
-    binding.pry
   end
 
   def self.create_from_filename(filename)
